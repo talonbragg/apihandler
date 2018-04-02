03531ff6ac7cac94cfa78886eb1316690fce4cd0
Talon Bragg <pvpgamecrash@gmail.com>
2018-04-02 16:32:24 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,24 +4,32 @@ var http = require('http');
 
 module.exports = function handler(apiurl) {
     //Handler code
-  
+
     var buffer = "",
         data,
         route,
-        promise = new Promise();
-    http.get(apiurl, function(response) {
-        // data is streamed in chunks from the server
-        // so we have to handle the "data" event
+        data = new Promise(function(resolve, reject) {
+            http.get(apiurl, function(response) {
+                // data is streamed in chunks from the server
+                // so we have to handle the "data" event
 
-        response.on("data", function(chunk) {
-            buffer += chunk;
-        });
+                response.on("data", function(chunk) {
+                    buffer += chunk;
+                }, function(err) {
+                    console.error(err)
+                });
 
-        response.on("end", function(err) {
-            // finished transferring data
-            // dump the raw data
-            promise.resolve(JSON.parse(buffer));
+                response.on("end", function(err) {
+                    // finished transferring data
+                    // dump the raw data
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                    }
+                    resolve(JSON.parse(buffer));
+                });
+            });
         });
-    });
-    return promise;
+    return data;
+
 }
