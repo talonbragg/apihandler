3982cc09492e4adb7378b80d1f9a00a0fb18bc27
Talon <pvpgamecrash@gmail.com>
2018-04-04 13:28:10 -0700

    more tests, code coverage

--- test/test.js
+++ test/test.js
@@ -5,9 +5,15 @@ const handler = require('../handler.js');
 
 describe('#apiData', function() {
     it('should return a json object', function() {
-        handler("https://sponge-impulse.glitch.me/finduser/?name=dabest")
+        handler("http://sponge-impulse.glitch.me/finduser/?name=dabest")
             .then(data => {
                 expect(data).to.be.an('object');
             })
       });
+    it('should return a string', function() {
+        handler('http://sponge-impulse.glitch.me/finduser/?name=dabest')
+            .then(data => {
+                expect(data.userName).to.be.a('string');
+            })
+    });
 });
